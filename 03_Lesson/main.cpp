157aee91ffa9855727f804ecf2968e772430940b
Gamze Efendioglu <gamze1087@gmail.com>
2020-09-17 23:04:16 +0300

    Updatiing 03_Lesson Project

--- 03_Lesson/main.cpp
+++ 03_Lesson/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
-int command = 45;
-
-int& refFunc() {
-	return command;
-}
-
 int main() {
 
-	refFunc() = 100; // refFunc() is a L-Value
-	std::cout << "command: " << command << "\n"; // command = 100
+	// Const Auto --> const is a type specifier
+	// ----------------------------------------------
+	int firstNumber = 10;
+	const auto x = firstNumber; // y --> const int 
+	auto const y = firstNumber; // z --> const int
+
+	// Const Auto Ref : 
+	// ----------------------------------------------
+	auto& refAuto = firstNumber;        // refAuto -->  int &
+	const auto &refConstAuto = refAuto; // refConstAuto --> const int& --> very common usage
+	
+	// Static Auto -->  static is not a type specifier
+	// ----------------------------------------------
+	static int staticNumber = 10;
+	auto staticNum = staticNumber; // staticNum --> int 
+	
 }
 
 	
