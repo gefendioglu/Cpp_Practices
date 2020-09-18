7291a9e0163da48f57d6be732e525f0bab5f46ee
Gamze Efendioglu <gamze1087@gmail.com>
2020-09-18 23:29:20 +0300

    Updating 03_Lesson Project

--- 03_Lesson/main.cpp
+++ 03_Lesson/main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 
+void func(int , int , int z=10);
+void foo1(int x = 30, int y =20 , int z=10);
+
+void foo2(int x, int y =20 , int);		   // Syntax Error
+void foo3(int x, int y =20 , int z = 10);  // Not Syntax Error
+
+
 int main() {
 
-	// Const Auto --> const is a type specifier
-	// ----------------------------------------------
-	int firstNumber = 10;
-	const auto x = firstNumber; // y --> const int 
-	auto const y = firstNumber; // z --> const int
+	func(10, 20, 30);  // OK --> z = 30
+	func(10, 20);      // OK --> z = 10
+	func(10);		   // NOT OK --> second argument shall be sent !
+	func();			   // NOT OK --> first and second arguments shall be sent !
+	func(1, 2, 3, 4);  // NOT OK --> No fourth parameters in function declaration
 
-	// Const Auto Ref : 
-	// ----------------------------------------------
-	auto& refAuto = firstNumber;        // refAuto -->  int &
-	const auto &refConstAuto = refAuto; // refConstAuto --> const int& --> very common usage
-	
-	// Static Auto -->  static is not a type specifier
-	// ----------------------------------------------
-	static int staticNumber = 10;
-	auto staticNum = staticNumber; // staticNum --> int 
+	foo1();       // OK --> foo1(30,20,10);
+	foo1(1);      // OK --> foo1(1,20,10);
+	foo1(1, 2);   // OK --> foo1(1,2,10);
+	foo1(1, 2,3); // OK --> foo1(1,2,3);
 	
 }
 
