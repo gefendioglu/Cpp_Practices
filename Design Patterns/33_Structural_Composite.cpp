4e0daf8b760d086bf497686aa2f08d5840233437
Gamze Efendioglu <gamze1087@gmail.com>
2020-12-05 00:22:56 +0300

    Composite & Facade Design Pattern

--- Design Patterns/33_Structural_Composite.cpp
+++ Design Patterns/33_Structural_Composite.cpp
@@ -27,82 +27,186 @@ public:
             objects->draw();
     }
 };
-
-int main() {
-
-    Group root("root");
-    root.m_objects.push_back(new Circle);
-
-    Group subgroup("sub");
-    subgroup.m_objects.push_back(new Circle);
-
-    root.m_objects.push_back(&subgroup);
-    root.draw();
-
-    return EXIT_SUCCESS;
-
-    /*
-        Group root contains:
-        Circle
-        Group sub contains:
-        Circle
-    */
-}
+//
+//int main() {
+//
+//    Group root("root");
+//    root.m_objects.push_back(new Circle);
+//
+//    Group subgroup("sub");
+//    subgroup.m_objects.push_back(new Circle);
+//
+//    root.m_objects.push_back(&subgroup);
+//    root.draw();
+//
+//    return EXIT_SUCCESS;
+//
+//    /*
+//        Group root contains:
+//        Circle
+//        Group sub contains:
+//        Circle
+//    */
+//}
 
 // ----------------------------------------------------
 // ----------------------------------------------------
 
-class Neuron {
+template <typename Self>
+class SomeNeurons {
 public:
+    template <typename T>
+    void connect_to(T& other);
+};
+
+class Neuron : public SomeNeurons<Neuron> {
+public:
+    std::vector<Neuron*> in, out;
+    uint32_t id;
+
     Neuron() {
         static int id = 1;
         this->id = id++;
     }
+    Neuron* begin() { return this; }
+    Neuron* end() { return this + 1; }
+};
 
-    void connect_to(Neuron& other) {
-        out.push_back(&other);
-        other.in.push_back(this);
+struct NeuronLayer : public std::vector<Neuron>, SomeNeurons<NeuronLayer> {
+public:
+    NeuronLayer(int count) {
+        while (count-- > 0)
+            emplace_back(Neuron{}); // temporary object
     }
+};
 
-    friend std::ostream& operator<<(std::ostream& os, const Neuron& obj) {
+template <typename Self>
+template <typename T>
+void SomeNeurons<Self>::connect_to(T& other) {
+    for (Neuron& from : *static_cast<Self*>(this)) {
+        for (Neuron& to : other) {
+            from.out.push_back(&to);
+            to.in.push_back(&from);
+        }
+    }
+}
+
+template <typename Self>
+std::ostream& operator<<(std::ostream& os, SomeNeurons<Self>& object) {
+    for (Neuron& obj : *static_cast<Self*>(&object)) {
         for (Neuron* n : obj.in)
             os << n->id << "\t-->\t[" << obj.id << "]" << "\n";
 
         for (Neuron* n : obj.out)
             os << "[" << obj.id << "]\t-->\t" << n->id << "\n";
+    }
+    return os;
+}
 
-        return os;
+//int main() {
+//    Neuron n1, n2;
+//    NeuronLayer l1{ 1 }, l2{ 2 };
+//
+//    n1.connect_to(l1); // Scenario 1: Neuron connects to Layer
+//    l2.connect_to(n2); // Scenario 2: Layer connects to Neuron
+//    l1.connect_to(l2); // Scenario 3: Layer connects to Layer
+//    n1.connect_to(n2); // Scenario 4: Neuron connects to Neuron
+//
+//    std::cout << "Neuron " << n1.id << "\n" << n1 << "\n";
+//    std::cout << "Neuron " << n2.id << "\n" << n2 << "\n";
+//
+//    std::cout << "Layer " << "\n" << l1 << "\n";
+//    std::cout << "Layer " << "\n" << l2 << "\n";
+//
+//    return EXIT_SUCCESS;
+//
+//
+//    /* Output
+//        Neuron 1
+//        [1]    -->    3
+//        [1]    -->    2
+//
+//        Neuron 2
+//        4    -->    [2]
+//        5    -->    [2]
+//        1    -->    [2]
+//
+//        Layer
+//        1    -->    [3]
+//        [3]    -->    4
+//        [3]    -->    5
+//
+//        Layer
+//        3    -->    [4]
+//        [4]    -->    2
+//        3    -->    [5]
+//        [5]    -->    2
+//    */
+//}
+
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+// Base Class
+class PageObject {
+public:
+    virtual void Add(PageObject a){}
+    virtual void Remove(){}
+    virtual void Delete(PageObject a){}
+};
+
+class Page : public PageObject {
+public:
+    void Add(PageObject a) override   {
+        std::cout << "something is added to the page" << "\n";
+    }
+    void Remove() override {
+        std::cout << "something is removed from the page" << "\n";
+    }
+    void Delete(PageObject a) override {
+        std::cout << "something is deleted from page " << "\n";
+    }
+};
+
+class Copy : public PageObject {
+public:
+    void AddElement(PageObject a)  {
+        copyPages.push_back(a);
+    }
+
+    void Add(PageObject a) override {
+        std::cout << "something is added to the copy" << "\n";
+    }
+
+    void Remove() override {
+        std::cout << "something is removed from the copy" << "\n";
+    }
+    void Delete(PageObject a) override{
+        std::cout << "something is deleted from the copy";
     }
 
 private:
-    std::vector<Neuron*> in, out;
-    uint32_t id;
+    // Both has-a (collection type) and is-a relationships
+    std::vector<PageObject> copyPages;
 };
 
-//  Making a neuron layer and it is basically like a collection.
-class NeuronLayer : public std::vector<Neuron> {
-public:
-    NeuronLayer(int count) {
-        while (count-- > 0)
-            emplace_back(Neuron{});
-    }
+//int main()
+//{
+//    Page a;
+//    Page b;
+//    Copy allcopy;
+//
+//    allcopy.AddElement(a);
+//    allcopy.AddElement(b);
+//
+//    allcopy.Add(a);   // something is added to the copy
+//    a.Add(b);         // something is added to the page
+//     
+//    allcopy.Remove(); // something is removed from the copy
+//    b.Remove();       // something is removed from the page
+//
+//    return 0;
+//}
 
-    friend std::ostream& operator<<(std::ostream& os, NeuronLayer& obj) {
-        for (auto& n : obj)
-            os << n;
-        return os;
-    }
-};
-
-int main() {
-
-    NeuronLayer l1{ 1 }, l2{ 2 };
-    Neuron n1, n2;
-    n1.connect_to(l1);  // Neuron connects to Layer
-    l2.connect_to(n2);  // Layer connects to Neuron
-    l1.connect_to(l2);  // Layer connects to Layer
-    n1.connect_to(n2);  // Neuron connects to Neuron
-
-    return EXIT_SUCCESS;
-}
-
+// ----------------------------------------------------
+// ----------------------------------------------------
