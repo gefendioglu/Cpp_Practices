151d3e27b41b1147ca67adf22c5fa6d758966c5c
Gamze Efendioglu <gamze1087@gmail.com>
2020-12-19 18:23:05 +0300

    Function Overloading

--- 05_Lesson/05_Lesson.cpp
+++ 05_Lesson/05_Lesson.cpp
@@ -1,9 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstdint>
 
+void func(const int &ref); // const L-Value reference
+void func(int &&ref);      // R-Value reference
 
-int main(){
+int main() {
 
-
-}
+	int x = 10;
+	func(10); // both functions are viable !!
+			  // R-Value ref. --> const L-Value ref. is OK !
+} 
 
