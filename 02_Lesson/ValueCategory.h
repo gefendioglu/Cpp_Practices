8622e139773fb2f7b4de1d8306c07ae91ff61194
Gamze Efendioglu <gamze1087@gmail.com>
2020-12-19 19:02:49 +0300

    File Arrangment

--- 02_Lesson/ValueCategory.h
+++ 02_Lesson/ValueCategory.h
@@ -16,4 +16,21 @@ struct ValueCategory<T&&> {
 	constexpr static const char* ptrstr{ "X Value" };
 };
 
+/*
 #define valuecategory(exp) (std::cout << "Value category of \"" #exp"\" is : " << ValueCategory<decltype((exp))>::ptrstr << "\n");
+
+
+#include <iostream>
+#include "ValueCategory.h"
+
+int main() {
+
+	int array[2] = { 0,1 };
+	sizeof(array[3]);
+
+	valuecategory(sizeof(array[3])); // Value category of "sizeof(array[3])" is : PR Value
+
+	std::cout << "sizeof(array[3]) :  " << sizeof(array[3]) << "\n";
+	// sizeof(array[3]) :  4
+}
+*/
---
+++ Cpp_Features/Cpp_Features.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Function_Overloading.h"
+
+#if 0
+
+#endif // 0
+
+#define Function_Overloading
+
+int main() {
+
+}
